4444b8201e6a26696027f6129e0f772426d95c4a
Phillip R. Berger <phillip@berger.land>
2022-11-03 16:01:29 -0400

    Continued to share layout

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,6 +1,8 @@
+import styles from '../../styles/Footer.module.scss'
+
 const Footer = () => {
     return (
-        <footer>Footer!</footer>
+        <footer className={styles.footer}>Footer!</footer>
     )
 }
 
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,13 +1,14 @@
+import styles from '../../styles/Layout.module.scss'
 import Header from '../header'
 import Footer from '../footer'
 
 const Layout = ({ children }: any) => {
     return (
-        <>
+        <div className={styles.parent}>
             <Header></Header>
             <main>{children}</main>
             <Footer></Footer>
-        </>
+        </div>
     )
 }
 
